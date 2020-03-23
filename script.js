f9b6652585292d3962f634959ce620de69f30c34
Forrest Olson <fs.olson5@gmail.com>
2020-03-22 23:37:51 -0600

    large overhaul, new features, history, days, wake

--- script.js
+++ script.js
@@ -3,14 +3,41 @@ var app = new Vue({
     data: {
         child: '',
         times: [],
+        prevDay: '',
+        currDay: '',
+        history: '',
+        index: 1,
     },
     mounted() {
+        // we can possibly eliminate currDay and just use new Date()
+        localStorage.currDay = moment(new Date()).format('MMM D');
         if (localStorage.times) {
             this.times = JSON.parse(localStorage.times);
         }
         if (localStorage.child) {
             this.child = localStorage.child;
         }
+        if (localStorage.index) {
+            this.index = parseInt(localStorage.index);
+        }
+        if (localStorage.currDay && localStorage.prevDay) {
+            if (localStorage.currDay != localStorage.prevDay) {
+                if (localStorage.history) {
+                    let myTimes = JSON.parse(localStorage.history);
+                    myTimes.push({day: this.index, events:this.times})
+                    localStorage.history = JSON.stringify(myTimes);
+                } else {
+                    localStorage.history = JSON.stringify([{day:this.index, events:this.times}]);
+                }
+                this.times = [];
+                this.index += 1;
+            }
+        } 
+        if (localStorage.history) {
+            this.history = JSON.parse(localStorage.history);
+        }
+        localStorage.prevDay = localStorage.currDay;
+        localStorage.index = this.index;
     },
     watch: {
         times(newNames) {
@@ -22,7 +49,7 @@ var app = new Vue({
     },
     methods: {
         addTime(action) {
-            let now = moment(new Date()).format("h:mm a");
+            let now = new Date();
             this.times.push({type:action,time:now});
             console.log(this.times);
             // this.$cookies.set("newKey1", this.times[0]);
@@ -36,21 +63,40 @@ var app = new Vue({
         },
         clearName() {
             this.child ='';
-        }
+        }, 
+        clearHistory() {
+            localStorage.history = JSON.stringify([]);
+            this.index = 1;
+            localStorage.index = this.index;
+            this.history = [];
+        },
+        viewHistory() {
+            let style = document.getElementById("history").style.display;
+            if (style != "none") {
+                document.getElementById("history").style.display = "none";
+            } else {
+                document.getElementById("history").style.display = "";
+            }
+            
+        },
     },
 
     computed: {
         printTimes() {
             return Array.from(this.times, time => {
-                let pastTense;
-                if (time.type === 'eat') {
-                    pastTense = 'ate';
+                if (time.type === 'woke up') {
+                    return time.type + ' at ' + moment(time.time).format("h:mm a") + 
+                        " (wake him up at " + moment(new Date(time.time)).add(75, "minutes").format('hh:mm a') + ")";
                 }
-                if (time.type === 'sleep') {
-                    pastTense = 'slept';
-                }
-                return pastTense + ' at ' + time.time
+                return time.type + ' at ' + moment(time.time).format("h:mm a");
             });
-        }
+        },
+        printHistory() {
+            return Array.from(this.history, day => {
+                return "Day " + day.day + " :" + Array.from(day.events, event => {
+                    return " " + event.type + " at " + moment(event.time).format('h:mm a');
+                });
+            });
+        },
     }
 });
